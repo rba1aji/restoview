0b178c57f28dae0ac7ddd101f74029bd84f2356f
rba1aji <rjibalaji@gmail.com>
2022-06-24 02:49:57 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 
 import routes from './routes';
 
-import { BrowserRouter , Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
@@ -11,20 +11,26 @@ export default function App() {
       <BrowserRouter>
         <nav>
           <ul>
-            
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/auth/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/auth/register">Register</Link>
+            </li>
           </ul>
         </nav>
-        {
-          routes.map(
-            (route,index)=>(
-              <router
-                path={route.path}
-                exact 
-                render={(props)=><route.component{...props}/>}
-              />
-            )
+        {routes.map(
+          (route, index) => (
+            <Route
+              path={route.path}
+              exact
+              // render={(props)=><route.component{...props}/>}
+            />
           )
-        }
+        )}
       </BrowserRouter>
     </div>
   );
