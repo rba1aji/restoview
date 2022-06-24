30fbfc6b49cbd877e5be9f6e9c4410137b44450c
rba1aji <rjibalaji@gmail.com>
2022-06-24 14:59:25 +0530

    NavBar updated

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import './style.css';
 import routes from './routes';
 import { BrowserRouter, Route, Link, Routes, Outlet } from 'react-router-dom';
 
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/filter">Filter</Link>
+          </li>
+          <li>
+            <Link to="/near-by-restaurants">Nearby Restaurants</Link>
+          </li>
+          <li>
+            <Link to="/top-rated-restaurants">Top Rated Restaurants</Link>
+          </li>
+          <li>
+            <Link to="/auth/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* <Outlet/> */}
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/filter">Filter</Link>
-            </li>
-            <li>
-              <Link to="/near-by-restaurants">Nearby Restaurants</Link>
-            </li>
-            <li>
-              <Link to="/top-rated-restaurants">Top Rated Restaurants</Link>
-            </li>
-            <li>
-              <Link to="/auth/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        {/* <Outlet/> */}
+        <NavBar />
         <Routes>
           {routes.map((route, index) => (
             <Route
