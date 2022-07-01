c05e6759ab49af75f9b5cfe8308ea190e30d5354
rba1aji <rjibalaji@gmail.com>
2022-07-02 00:25:10 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect , useState } from 'react';
 import axios from 'axios';
 
 export default function Search() {
+  const [input,setInput]=useState("");
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
   // const searchUrl = `https://{baseURL}/search/{versionNumber}/search/{query}.json?key={Your_API_Key}&limit={limit}&categorySet={categorySet}`;
   const URL = `https://api.tomtom.com/search/2/search/ammanhotel.json?catagorySet=restaurant&key=${KEY}`;
+  
+  var config = {
+    method: 'get',
+    url: URL,
+    headers: {},
+  };
+   
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-  axios
-    .get(URL)
-    .then((res) => {
-      console.log(JSON.stringify(res.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then(function () {});
+
   return (
     <>
-      <input />
+      <input
+      placeholder="Search..."
+      />
     </>
   );
 }
