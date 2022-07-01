8ef970e446c5fb6dc7dfaba43d76bea882903aad
rba1aji <rjibalaji@gmail.com>
2022-07-02 00:42:09 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,37 @@
-import React, { useEffect , useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Search() {
-  const [input,setInput]=useState('');
-  const [options,setOptions]=useState([]);
+  const [input, setInput] = useState('amman');
+  const [options, setOptions] = useState([]);
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
   // const searchUrl = `https://{baseURL}/search/{versionNumber}/search/{query}.json?key={Your_API_Key}&limit={limit}&categorySet={categorySet}`;
   const URL = `https://api.tomtom.com/search/2/search/${input}.json?catagorySet=restaurant&key=${KEY}`;
-  
+
   var config = {
     method: 'get',
     url: URL,
     headers: {},
   };
-   
+
   useEffect(() => {
     axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
+      .then((res) => {
+        const s=JSON.stringify(res.data);
+        // JSON.stringify(res.data);
+        console.log(res);
+        // setOptions(JSON)
       })
       .catch(function (error) {
         console.log(error);
       });
   });
 
-
   return (
     <>
       <input
         placeholder="Search..."
-        onChange={
-          (e)=>setInput(e.target.value)
-        }
+        onChange={(e) => setInput(e.target.value)}
       />
     </>
   );
