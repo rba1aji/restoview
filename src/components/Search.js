b447767d6ba8ce3fc8f2deae66670a7e0b59ab24
rba1aji <rjibalaji@gmail.com>
2022-07-02 01:25:55 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,10 @@ export default function Search() {
   const [input, setInput] = useState('');
   const [options, setOptions] = useState([]);
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
-  // const searchUrl = `https://{baseURL}/search/{versionNumber}/search/{query}.json?key={Your_API_Key}&limit={limit}&categorySet={categorySet}`;
-  const URL = `https://api.tomtom.com/search/2/search/${input}.json?catagorySet=restaurant&key=${KEY}`;
+  // const searchUrl = https://{baseURL}/search/{versionNumber}/autocomplete/{query}.{ext}?key={Your_API_Key}&language={language}&limit={limit}&lat={lat}&lon={lon}&radius={radius}&countrySet={countrySet}&resultSet={resultSet}
+  // curl 'https://{baseURL}/search/{versionNumber}/search/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&countrySet={countrySet}&lat={lat}&lon={lon}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&idxSet={idxSet}&extendedPostalCodesFor={extendedPostalCodesFor}&minFuzzyLevel={minFuzzyLevel}&maxFuzzyLevel={maxFuzzyLevel}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}&entityTypeSet={entityTypeSet}'
+  //curl 'https://{baseURL}/search/{versionNumber}/categorySearch/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&lat={lat}&lon={lon}&countrySet={countrySet}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&extendedPostalCodesFor={extendedPostalCodesFor}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}'
+  const URL = `https://api.tomtom.com/search/2/search/${input}.json?categorySet=7315&key=${KEY}`;
 
   var config = {
     method: 'get',
@@ -14,23 +16,30 @@ export default function Search() {
     headers: {},
   };
 
+  function HandleChange(e) {
+    setInput(e.target.value);
+    console.log(input);
+  }
+
   useEffect(() => {
     axios(config)
       .then((res) => {
         // const s=JSON.stringify(res.data);
-        console.log(res); 
-        // setOptions(JSON)
+        console.log(res);
       })
       .catch(function (error) {
         console.log(error);
       });
-  },[input]);
+  }, [input]);
 
   return (
     <>
       <input
         placeholder="Search..."
-        onChange={(e) => setInput(e.target.value)}
+        onChange={
+          HandleChange
+          //(e) => setInput(e.target.value)
+        }
       />
     </>
   );
