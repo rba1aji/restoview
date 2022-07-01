2c1710c66b1a0ace39f8d9922c5f6c1ff6392dae
rba1aji <rjibalaji@gmail.com>
2022-07-02 00:31:34 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,11 @@ import React, { useEffect , useState } from 'react';
 import axios from 'axios';
 
 export default function Search() {
-  const [input,setInput]=useState("");
+  const [input,setInput]=useState('');
+  const [options,setOptions]=useState([]);
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
   // const searchUrl = `https://{baseURL}/search/{versionNumber}/search/{query}.json?key={Your_API_Key}&limit={limit}&categorySet={categorySet}`;
-  const URL = `https://api.tomtom.com/search/2/search/ammanhotel.json?catagorySet=restaurant&key=${KEY}`;
+  const URL = `https://api.tomtom.com/search/2/search/${input}.json?catagorySet=restaurant&key=${KEY}`;
   
   var config = {
     method: 'get',
@@ -21,13 +22,16 @@ export default function Search() {
       .catch(function (error) {
         console.log(error);
       });
-  }, []);
+  });
 
 
   return (
     <>
       <input
-      placeholder="Search..."
+        placeholder="Search..."
+        onChange={
+          (e)=>setInput(e.target.value)
+        }
       />
     </>
   );
