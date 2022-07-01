a6b829493cce4e93b777be962b54a48cfb802eac
rba1aji <rjibalaji@gmail.com>
2022-07-01 21:24:16 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 export default function Search() {
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
+  const baseUrl = ``;
+  const searchUrl = ``;
   return (
     <>
       <input />
