d49382cc3ef49aee3398cc75f0d8cee7f052b639
rba1aji <rjibalaji@gmail.com>
2022-07-02 17:14:26 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 export default function Search() {
-  const [input, setInput] = useState('');
+  const inputRef = useRef();
   const [options, setOptions] = useState([]);
   const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
-  // const searchUrl = https://{baseURL}/search/{versionNumber}/autocomplete/{query}.{ext}?key={Your_API_Key}&language={language}&limit={limit}&lat={lat}&lon={lon}&radius={radius}&countrySet={countrySet}&resultSet={resultSet}
-  // curl 'https://{baseURL}/search/{versionNumber}/search/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&countrySet={countrySet}&lat={lat}&lon={lon}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&idxSet={idxSet}&extendedPostalCodesFor={extendedPostalCodesFor}&minFuzzyLevel={minFuzzyLevel}&maxFuzzyLevel={maxFuzzyLevel}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}&entityTypeSet={entityTypeSet}'
-  //curl 'https://{baseURL}/search/{versionNumber}/categorySearch/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&lat={lat}&lon={lon}&countrySet={countrySet}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&extendedPostalCodesFor={extendedPostalCodesFor}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}'
-  const URL = `https://api.tomtom.com/search/2/search/${input}.json?categorySet=7315&key=${KEY}`;
+  let URL = undefined;
 
-  function HandleChange(e) {
-    setInput(e.target.value);
-    console.log(input);
-  
-    axios.get(URL)
+  function HandleInputChange(e) {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    URL = `https://api.tomtom.com/search/2/search/${inputRef.current.value}.json?categorySet=7315&key=${KEY}`;
+    axios
+      .get(URL)
       .then((res) => {
-        // const s=JSON.stringify(res.data);
         console.log(res);
       })
       .catch(function (error) {
         console.log(error);
       });
-    }
+  }
 
   return (
     <>
       <input
         placeholder="Search..."
-        onChange={
-          HandleChange
-          //(e) => setInput(e.target.value)
-        }
+        ref={inputRef}
+        onChange={HandleInputChange}
       />
     </>
   );
