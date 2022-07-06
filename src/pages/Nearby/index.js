bc0ce974bbf366c688e51cd72f03c320b334be78
rba1aji <rjibalaji@gmail.com>
2022-07-06 17:54:52 +0530

    fixing css

--- src/pages/Nearby/index.js
+++ src/pages/Nearby/index.js
@@ -4,21 +4,19 @@ export default function Nearby() {
   return (
     <>
       <h1>Nearby Restaurants</h1>
-      <Form
-        className="m-4 "
+      <Form className="mt-4"
         style={{
-          display: 'grid',
-          justifyContent: 'center',
+          display:'grid',
+          marginLeft:"15vw",
+          marginRight:"15vw"
         }}
       >
-        <Button variant="outline-dark">Use Current Location</Button>
-        <p className="text-center m-3">or</p>
+        <Button variant="outline-dark" size="md" className="ms-1 me-1">Use Current Location</Button>
+        <p className="text-center m-2">or</p>
         <Form.Group>
-          <p className="m-0 text-center">
-            Enter location manually
-          </p>
+          <p className="mb-1 text-center">Enter location manually</p>
           <Form.Control
-            className="border border-3"
+            className="border-dark"
             type="text"
             placeholder="your location..?"
           />
