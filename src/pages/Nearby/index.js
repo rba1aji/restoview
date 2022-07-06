a5832184e688e903fad5e443246b4bc9e61ed349
rba1aji <rjibalaji@gmail.com>
2022-07-06 20:40:54 +0530

    working on cuurent location detect

--- src/pages/Nearby/index.js
+++ src/pages/Nearby/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FormControl, Button, Form } from 'react-bootstrap';
+
+function AutoLocationDetect() {
+  const URL = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${API_KEY}&lat=${lat}&lon=${lon}&countrySet=IN&categoryset=7315&view=IN`;
+}
+
+function ManualLocationDetect(props) {
+  const URL = ``;
+}
+
 export default function Nearby() {
+  const currLocationRef = useRef();
   return (
     <>
-      <h1>Nearby Restaurants</h1>
-      <Form className="mt-4"
+      <div
+        className="mt-4 mb-5"
         style={{
-          display:'grid',
-          marginLeft:"15vw",
-          marginRight:"15vw"
+          display: 'grid',
+          marginLeft: '15vw',
+          marginRight: '15vw',
         }}
       >
-        <Button variant="outline-dark" size="md" className="ms-1 me-1">Use current location</Button>
+        <Button variant="outline-dark" size="md" className="ms-1 me-1">
+          Use current location
+        </Button>
         <p className="text-center m-2">or</p>
         <Form.Group>
           <p className="mb-1 text-center">Enter location manually</p>
@@ -19,9 +31,14 @@ export default function Nearby() {
             className="border-dark"
             type="text"
             placeholder="your location..?"
+            Ref={currLocationRef}
+            onChange={(e) => {
+              <ManualLocationDetect place={currLocation.current.value} />;
+            }}
           />
         </Form.Group>
-      </Form>
+      </div>
+      <h1>Nearby Restaurants</h1>
     </>
   );
 }
