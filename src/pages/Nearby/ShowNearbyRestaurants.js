71b34adfd1caa8f6df65eb05c1e25eea7f89c97f
rba1aji <rjibalaji@gmail.com>
2022-07-07 09:43:24 +0530

    working on nearby list

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import API_KEY from '../../components/GetAPIKey';
+/*data: Object
+results: Array[10]
+0: Object
+address: Object
+freeformAddress: "18 North Cutcherry Street, Opp New Anjaneyar Temple Old Bus Stand, Rasipuram 637401, Tamil Nadu"
+id: "356009039102242"
+info: "search:ta:356009039102242-IN"
+poi: Object
+categories: Array[2] //tags
+0: "indian"
+1: "restaurant"
+name: "Vaagai Restaurant" 
 
+*/
 export default function (props) {
   const [nearbyList, setNearbyList] = useState([]);
 
