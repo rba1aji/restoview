c6b6cf46ca32564f2440d94ebae62a390e6c3a55
rba1aji <rjibalaji@gmail.com>
2022-07-07 08:41:55 +0530

    showNearbyRestaurant - shows non restauarants:(

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -1,4 +1,17 @@
-import React from 'react';
-export default function (res) {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import API_KEY from '../../components/GetAPIKey';
+export default function (props) {
+  const NearbyUrl = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${API_KEY}&${props.latlon}&countrySet=IN&categoryset=7315&view=IN`;
+  useEffect(() => {
+    axios
+      .get(NearbyUrl)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [props.latlon]);
   return;
 }
