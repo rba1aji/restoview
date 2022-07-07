59a64aba6196dc391bac237b9cec4a755cf34a7f
rba1aji <rjibalaji@gmail.com>
2022-07-07 08:52:53 +0530

    countryset -> countrySet

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import API_KEY from '../../components/GetAPIKey';
 export default function (props) {
-  const NearbyUrl = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${API_KEY}&${props.latlon}&countrySet=IN&categoryset=7315&view=IN`;
+  const NearbyUrl = `https://api.tomtom.com/search/2/nearbySearch/.json?key=${API_KEY}&${props.latlon}&countrySet=IN&categoryet=7315&view=IN`;
   useEffect(() => {
     axios
       .get(NearbyUrl)
