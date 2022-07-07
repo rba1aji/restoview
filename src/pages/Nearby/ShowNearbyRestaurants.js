a6db7e6484bab7c842900e143a9351327356e0c2
rba1aji <rjibalaji@gmail.com>
2022-07-07 09:55:22 +0530

    setting Nearby List

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import API_KEY from '../../components/GetAPIKey';
 /*data: Object
-results: Array[10]
-0: Object
-address: Object
-freeformAddress: "18 North Cutcherry Street, Opp New Anjaneyar Temple Old Bus Stand, Rasipuram 637401, Tamil Nadu"
+results[0].
+address.
+freeformAddress: ""
 id: "356009039102242"
-info: "search:ta:356009039102242-IN"
-poi: Object
+poi.
 categories: Array[2] //tags
 0: "indian"
 1: "restaurant"
 name: "Vaagai Restaurant" 
-
+openingHours
+phone
 */
 export default function (props) {
   const [nearbyList, setNearbyList] = useState([]);
@@ -24,7 +23,11 @@ export default function (props) {
     axios
       .get(NearbyUrl)
       .then((res) => {
-        console.log(res);
+        // console.log(res);
+        const details={};
+        setNearbyList((old)=>{
+          return[...old, ]
+        })
       })
       .catch((err) => {
         console.log(err);
