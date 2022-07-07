6070c243157ce94c5631f45dc24f9fad846e76dc
rba1aji <rjibalaji@gmail.com>
2022-07-07 22:31:18 +0530

    card somewhat super

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 export default function Login() {
   return (
     <div className="text-cente">
@@ -11,15 +11,32 @@ export default function Login() {
         <Card>
           <Card.Img variant="top" src="" />
           <Card.Body>
-            <Card.Title className="h2 mb-1">Balaji hotel</Card.Title>
-            <Card.Text className="mb-2" style={{ fontSize: 12 }}>
+            <Card.Title className="h2 mb-1">
+              <h2>Balaji hotel</h2>
+            </Card.Title>
+            <Card.Text className="mb-0" style={{ fontSize: 12 }}>
               1/20, Mariyamman temple street, Pillanallur, Rasipuram, Namakkal,
               TamilNadu.
             </Card.Text>
-            <Card.Text>Rating: ⭐⭐⭐⭐</Card.Text>
+            <button disabled className="pt-0 pb-0 border-0 mt-0" style={{fontSize:12}}>Veg</button>
+            <Card.Text className="">Rating: ⭐⭐⭐⭐</Card.Text>
             <Row>
-              <Col><Button>Call</Button></Col>
-              <Col><Button>Order</Button></Col>
+              <Col>
+                {true && (
+                  <Button variant="secondary" className="pt-0 pb-0 ps-4 pe-4">
+                    Call
+                  </Button>
+                )}
+              </Col>
+              <Col>
+                <Button
+                  style={{ float: 'right' }}
+                  variant="secondary"
+                  className="pt-0 pb-0 ps-3 pe-3"
+                >
+                  Order
+                </Button>
+              </Col>
             </Row>
           </Card.Body>
         </Card>
--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 
 export default function ShowNearbyRestaurants(props) {
   console.log(props.nearbyList);
@@ -9,13 +9,39 @@ export default function ShowNearbyRestaurants(props) {
         {props.nearbyList.map((item) => {
           return (
             <Col>
-              <Card>
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                  <Card.Title className="mb-0">{item.name}</Card.Title>
-                  <Card.Text style={{ fontSize: 10 }}>{item.address}</Card.Text>
-                </Card.Body>
-              </Card>
+            <Card>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title>
+              <h2 className="mb-0">{item.name}</h2>
+            </Card.Title>
+            <Card.Text className="mb-0" style={{ fontSize: 12 }}>
+              {item.address}
+            </Card.Text>
+            {item.tags.map((tag)=>{
+              return <button disabled className="pt-0 pb-0 border-0 mt-0" style={{fontSize:12}}>{tag}</button>; 
+            })}
+            <Card.Text className="">Rating: ⭐⭐⭐⭐</Card.Text>
+            <Row>
+              <Col>
+                {true && (
+                  <Button variant="secondary" className="pt-0 pb-0 ps-4 pe-4">
+                    Call
+                  </Button>
+                )}
+              </Col>
+              <Col>
+                <Button
+                  style={{ float: 'right' }}
+                  variant="secondary"
+                  className="pt-0 pb-0 ps-3 pe-3"
+                >
+                  Order
+                </Button>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
             </Col>
           );
         })}
