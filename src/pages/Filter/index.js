c8bf35c2a0e7336718009bbfd953084fba9ff313
rba1aji <rjibalaji@gmail.com>
2022-07-07 14:38:55 +0530

    1 file modified

--- src/pages/Filter/index.js
+++ src/pages/Filter/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 export default function Filter() {
-  return <h1>Filter</h1>;
+  return (
+    <>
+      <h1>Filter</h1>
+      <p className="text-center opacity-75">Find the restaurant you want</p>
+    </>
+  );
 }
