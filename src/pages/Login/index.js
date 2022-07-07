8c435c908f2561d1b7bbb9fca23211af3d50bddf
rba1aji <rjibalaji@gmail.com>
2022-07-07 19:35:55 +0530

    styling card

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Card} from 'react-bootstrap'
+import { Card, Row, Col } from 'react-bootstrap';
 export default function Login() {
   return (
     <div className="text-cente">
       <h1>Login</h1>
       <Link to="/auth/register">Register here</Link>
 
-
       <div className="m-5">
-      <Card>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title className="h2 mb-1">Balaji hotel</Card.Title>
-          <Card.Text className="mb-2" style={{ fontSize: 12}}>1/20, Mariyamman temple street, Pillanallur, Rasipuram, Namakkal, TamilNadu.</Card.Text>
-          <Card.Text>Rating: ⭐⭐⭐⭐</Card.Text>
-
-        </Card.Body>
-      </Card>
+        <Card>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title className="h2 mb-1">Balaji hotel</Card.Title>
+            <Card.Text className="mb-2" style={{ fontSize: 12 }}>
+              1/20, Mariyamman temple street, Pillanallur, Rasipuram, Namakkal,
+              TamilNadu.
+            </Card.Text>
+            <Card.Text>Rating: ⭐⭐⭐⭐</Card.Text>
+            <Row>
+              <Col><Button>Call</Button></Col>
+              <Col><Button>Order</Button></Col>
+            </Row>
+          </Card.Body>
+        </Card>
       </div>
-
-
     </div>
   );
 }
