82f5e11674c1021845cbaabf72e4625becf8a4ee
rba1aji <rjibalaji@gmail.com>
2022-07-07 13:33:05 +0530

    details taken from nearby api

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import API_KEY from '../../components/GetAPIKey';
-/*data: Object
-results[0].
-address.
-freeformAddress: ""
-id: "356009039102242"
-poi.
-categories: Array[2] //tags
-0: "indian"
-1: "restaurant"
-name: "Vaagai Restaurant" 
-openingHours
-phone
-*/
+
 export default function (props) {
   const [nearbyList, setNearbyList] = useState([]);
 
@@ -23,8 +11,12 @@ export default function (props) {
     setNearbyList([]);
     arr.map((item) => {
       const details = {
+        id:item.id,
         name: item.poi.name,
         address: item.address.freeformAddress,
+        phone:item.poi.phone,
+        tags: item.poi.categories,
+        openingHours:item.openingHours
       };
       setNearbyList((old) => {
         return [...old, details];
@@ -37,7 +29,6 @@ export default function (props) {
     axios
       .get(nearbyUrl)
       .then((res) => {
-        // console.log(res);
         MakeNearbyList(res.data.results);
       })
       .catch((err) => {
