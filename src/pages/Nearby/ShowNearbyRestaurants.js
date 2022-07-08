402aed9db8fe66988d329f0242d86df2b9201f46
rba1aji <rjibalaji@gmail.com>
2022-07-08 23:59:16 +0530

    order online link updated

--- src/pages/Nearby/ShowNearbyRestaurants.js
+++ src/pages/Nearby/ShowNearbyRestaurants.js
@@ -54,6 +54,7 @@ export default function ShowNearbyRestaurants(props) {
                           className="pt-0 pb-0 ps-3 pe-3"
                           as="a"
                           href={`https://www.swiggy.com/search?query=${item.name.replaceAll(' ','+')}`}
+                          target='_blank'
                         >
                           Order online
                         </Button>
