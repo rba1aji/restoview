44ad0a12b1f1f5e727373efafac7b8470f78b1aa
rba1aji <rjibalaji@gmail.com>
2022-07-08 12:08:01 +0530

    loader fixed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
 export default function Loader(props) {
-  if(!props.flag)return;
+  if (!props.flag) return;
   return (
     <div
-          style={{
-            position: 'fixed',
-            top: '45vh',
-            paddingLeft: '47vw',
-            zIndex: 1,
-            // height: '100%',
-            width: '100%',
-            zIndex: '9999',
-            // background:'black'
-          }}
-        >
-    <Spinner
-      animation="border"
-      role="status"
+      style={{
+        position: 'fixed',
+        top: '45vh',
+        left: '45vw',
+        zIndex: 1,
+        width: '100%',
+        zIndex: '9999',
+      }}
     >
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+      <div
+        style={{
+          position: 'absolute',
+          width: '10vw',
+          height: '10vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          // background: 'black',
+        }}
+      >
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
     </div>
   );
 }
