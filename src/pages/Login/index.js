c30cff7e561d381887a4dc0334c8f17b3f9e6117
rba1aji <rjibalaji@gmail.com>
2022-07-10 09:18:05 +0530

    working on Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Row, Col, Button } from 'react-bootstrap';
+import { Card, Row, Col, Button, Form } from 'react-bootstrap';
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function HandleLogin(e) {
+    // e.preventDefault();
+  }
   return (
     <div>
       <h1>Login</h1>
       <Link to="/auth/register">
         <p className="text-center">Register here</p>
       </Link>
+      <Form
+        className="d-flex-inline mx-auto"
+        style={{ width: '21rem' }}
+        onSubmit={HandleLogin}
+      >
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            className="border-dark"
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            className="border-dark"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+          />
+        </Form.Group>
+        <div className="text-center">
+          <Button className="px-4 my-3" variant="dark" type="submit">
+            Login
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 }
