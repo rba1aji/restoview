5c49476c863e6d772899012119e5883bc2acf7e9
rba1aji <rjibalaji@gmail.com>
2022-07-10 10:13:07 +0530

    firebase config added

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBlgfSceXNIIwszeaOWHPDd4yt1sW0PjYQ',
+  authDomain: 'resrat-1922b.firebaseapp.com',
+  projectId: 'resrat-1922b',
+  storageBucket: 'resrat-1922b.appspot.com',
+  messagingSenderId: '988646319782',
+  appId: '1:988646319782:web:fd491dcddf17142b263667',
+};
+
+const app = initializeApp(firebaseConfig);
