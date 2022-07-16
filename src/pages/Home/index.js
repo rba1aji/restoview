f4e5f81ecd5154fb6c441e5b4f9dfd4bdb0d69ac
rba1aji <rjibalaji@gmail.com>
2022-07-16 12:04:13 +0530

    styling home for desktop

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Search from './Search';
 // import '../../assets/chicken.jpg'
 
-function MobileComponent(){
-  return(
+function MobileComponent() {
+  return (
     <div>
       <Search />
-      <div
-        className="mobBanner1Container"
-      >
+      <div className="mobBanner1Container">
         <img
           style={{ width: '100%', borderRadius: '5px' }}
           src="https://raw.githubusercontent.com/rba1aji/my-assets/main/chicken1.jpg"
@@ -22,25 +20,27 @@ function MobileComponent(){
   );
 }
 
-function DesktopComponent(){
+function DesktopComponent() {
   return (
-    <div
-    className="mobBanner1Container"
-  >
-    <img
-      style={{ width: '100%', borderRadius: '5px' }}
-      src="https://raw.githubusercontent.com/rba1aji/my-assets/main/chicken1.jpg"
-    />
-    <div className="h2 mb-0 mobBanner1text">
-      What you eat there?
-      <br /> Rate it here!
+    <div className="dskBanner1Container">
+      <img
+        style={{ width: '100%', borderRadius: '5px', maxHeight: '220px' }}
+        src="https://raw.githubusercontent.com/rba1aji/my-assets/main/chicken1.jpg"
+      />
+      <div>
+        <div className="h2 mb-0 dskBanner1text">
+          What you eat there?
+          <br />Rate it here!
+        </div>
+        <div className="dskBanner1search">
+        <Search/>
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
 
 function Home() {
-  return window.innerWidth<=300? <MobileComponent/>:<DesktopComponent/>;
+  return window.innerWidth <= 300 ? <MobileComponent /> : <DesktopComponent />;
 }
 export default Home;
-
