a865fd25230a6597d800edc85ac05e21a32a5f51
rba1aji <rjibalaji@gmail.com>
2022-07-17 10:46:44 +0530

    making restaurant page

--- src/pages/RestaurantPage/index.js
+++ src/pages/RestaurantPage/index.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 const KEY = `O1W6gyHOMcvAfFFPGxQOGR2mBzWUAH2P`;
 
 export default function SelectedRestaurant() {
-  // const placeByIdUrl = `https://api.tomtom.com/search/2/place.json?entityId=${encodeURIComponent(
-  //   props.id
-  // )}&key=${KEY}&view=IN`;
-  // useEffect(() => {
-  //   axios
-  //     .get(placeByIdUrl)
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, [props.id]);
+  const { id } = useParams();
+  const [res, setRes] = useState();
+  const placeByIdUrl = `https://api.tomtom.com/search/2/place.json?entityId=${id}&key=${KEY}&view=IN`;
+
+  async function FetchResData() {
+    await axios
+      .get(placeByIdUrl)
+      .then((res) => {
+        setRes(res.data.results[0]);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    FetchResData();
+  }, []);
+
   return <p>s</p>;
 }
