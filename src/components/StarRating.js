e00ec146b71d24cff7a18c2040f74971a8e1106f
rba1aji <rjibalaji@gmail.com>
2022-07-17 15:33:23 +0530

    FirebaseError {code: "permission-denied",

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
+import { db } from '../configs/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
 
 export default function StarRating(props) {
+  const docRef = doc(db, 'restaurants', '0');
+
+  getDoc(docRef)
+    .then((doc) => {
+      if (doc.exists) {
+        console.log('Document data:', doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    })
+    .catch((error) => {
+      console.log('Error getting document:', error);
+    });
+
   const stars = [];
   for (let i = 0; i < 4; i++) {
     stars.push(<span>⭐</span>);
