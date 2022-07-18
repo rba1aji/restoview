020c5632d524aa92dd4dd8f0716de739721f8b00
rba1aji <rjibalaji@gmail.com>
2022-07-19 01:14:13 +0530

    1 file modified Too many re-renders

--- src/pages/Famous/index.js
+++ src/pages/Famous/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function TopRated() {
+  return <div>hro</div>;
+}
+function MostViewed() {
+  return <div>mostviewed</div>;
+}
 
 export default function TopRated() {
+  const [famousType, setFamousType] = useState(<p>d</p>);
   return (
     <>
       <h1>Famous Restaurants</h1>
@@ -13,19 +19,28 @@ export default function TopRated() {
         variant="tabs"
         defaultActiveKey="0"
         // className='border-3'
-        style={{ marginLeft: '10vw', marginRight: '10vw', marginTop: '10vh' }}
+        style={{ marginLeft: '10vw', marginRight: '10vw', marginTop: '5vh' }}
       >
         <Nav.Item>
-          <Nav.Link className="text-reset" eventKey="0">
+          <Nav.Link
+            className="text-reset"
+            eventKey="0"
+            onSelect={setFamousType(0)}
+          >
             Top Rated
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="text-reset" eventKey="1">
+          <Nav.Link
+            className="text-reset"
+            eventKey="1"
+            onSelect={setFamousType(1)}
+          >
             Most Viewed
           </Nav.Link>
         </Nav.Item>
       </Nav>
+      {famousType === 0 ? <TopRated /> : <MostViewed />}
     </>
   );
 }
