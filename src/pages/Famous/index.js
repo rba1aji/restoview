979f3b140c55329095d8ccf82dfc330a4b5f5688
rba1aji <rjibalaji@gmail.com>
2022-07-18 22:55:06 +0530

    1 file modified

--- src/pages/Famous/index.js
+++ src/pages/Famous/index.js
@@ -4,19 +4,28 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function TopRated() {
-    return (
-      <>
-        <h1>Famous Restaurants</h1>
+  return (
+    <>
+      <h1>Famous Restaurants</h1>
 
-    <Nav fill variant="tabs" style={{marginLeft:'10vw', marginRight:'10vw', marginTop:'10vh'}}>
-      <Nav.Item>
-        <Nav.Link eventKey="/">Top Rated</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="">Most Viewed</Nav.Link>
-      </Nav.Item>
-    </Nav>
-
+      <Nav
+        fill
+        variant="tabs"
+        defaultActiveKey="0"
+        // className='border-3'
+        style={{ marginLeft: '10vw', marginRight: '10vw', marginTop: '10vh' }}
+      >
+        <Nav.Item>
+          <Nav.Link className="text-reset" eventKey="0">
+            Top Rated
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="text-reset" eventKey="1">
+            Most Viewed
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
   );
 }
