3286c0973e66f5be22698cca66a42f9b30347f54
rba1aji <rjibalaji@gmail.com>
2022-07-18 22:46:59 +0530

    famous page making

--- src/pages/Famous/index.js
+++ src/pages/Famous/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 export default function TopRated() {
-  return (
-    <>
-      <h1>Famous Restaurants</h1>
+    return (
+      <>
+        <h1>Famous Restaurants</h1>
+
+    <Nav fill variant="tabs" style={{marginLeft:'10vw', marginRight:'10vw', marginTop:'10vh'}}>
+      <Nav.Item>
+        <Nav.Link eventKey="/">Top Rated</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="">Most Viewed</Nav.Link>
+      </Nav.Item>
+    </Nav>
+
     </>
   );
 }
