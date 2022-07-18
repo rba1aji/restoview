a1cace78887b6ceaf235f08872fef0fb7b888fd1
rba1aji <rjibalaji@gmail.com>
2022-07-19 02:01:55 +0530

    1 file modified

--- src/pages/Famous/index.js
+++ src/pages/Famous/index.js
@@ -14,31 +14,31 @@ export default function Famous() {
   return (
     <>
       <h1>Famous Restaurants</h1>
-      <div 
-        style={{ marginLeft: '10vw', marginRight: '10vw', marginTop: '5vh' }}
+      <div
+        style={{ marginLeft: '7vw', marginRight: '7vw', marginTop: '5vh' }}
         // className='border-dark border'
-        >
-      <Nav
-        fill
-        variant="tabs"
-        defaultActiveKey="0"
-        className=" "
-        onSelect={(key) => setFamousType(key)}
       >
-        <Nav.Item>
-          <Nav.Link className="text-reset" eventKey="0">
-            Top Rated
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link className="text-reset" eventKey="1">
-            Most Viewed
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+        <Nav
+          fill
+          variant="tabs"
+          defaultActiveKey="0"
+          className=" "
+          onSelect={(key) => setFamousType(key)}
+        >
+          <Nav.Item>
+            <Nav.Link className="text-reset" eventKey="0">
+              Top Rated
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="text-reset" eventKey="1">
+              Most Viewed
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
 
-      {famousType == 0 ? <TopRated /> : <MostViewed />}
-    </div>
+        {famousType == 0 ? <TopRated /> : <MostViewed />}
+      </div>
     </>
   );
 }
