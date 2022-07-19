a45396724cca2b9e7b03fb5058a6b3336577ef89
rba1aji <rjibalaji@gmail.com>
2022-07-19 17:33:10 +0530

    1 file added

---
+++ src/pages/RestaurantPage/FetchCloudDataByAPIData.js
@@ -0,0 +1,59 @@
+import { db } from '../configs/firebaseConfig';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import API_KEY from './API_KEY';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { AppState } from './AppContext';
+
+export default function FetchCloudData(APIData) {
+  const [cloudData, setCloudData] = useState();
+  const docRef = doc(db, 'restaurants', APIData.id);
+
+  function HandleUndefined() {
+    const newDocData = {
+      views: 0,
+      ratings: {
+        star: 5,
+        collection: {
+          overall: [{}],
+          food: [{}],
+          service: [{}],
+          quality: [{}],
+          valueForMoney: [{}],
+        },
+      },
+      reviews: [{}],
+      address: APIData.address,
+      openingHours: APIData.openingHours ? APIData.openingHours : null,
+      photos: [{}],
+    };
+
+    setDoc(docRef, newDocData)
+      .then((res) => {
+        FetchData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function FetchData() {
+    getDoc(docRef)
+      .then((res) => {
+        if (res.data()) {
+          setCloudData(res.data());
+        } else {
+          HandleUndefined();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
+  return cloudData;
+}
