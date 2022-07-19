2784dfbabeaab7c8f35f50b14b41f52ab818274e
rba1aji <rjibalaji@gmail.com>
2022-07-19 17:28:14 +0530

    1 file modified

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -6,67 +6,66 @@ import { Rating } from 'react-simple-star-rating';
 import { restoDocRef, FetchCloudData } from '../reducers/constants';
 
 export default function StarRating(props) {
-  const { cloudData, setCloudData } = AppState();
+  // const { cloudData, setCloudData } = AppState();
   // const docRef = doc(db, 'restaurants', props.resto.id);
-  const docRef = restoDocRef(props.resto.id);
+  // const docRef = restoDocRef(props.resto.id);
 
   const t=FetchCloudData(props.resto);
   // console.log(t);
 
-  function HandleUndefined() {
-    const newDocData = {
-      views: 0,
-      ratings: {
-        star: 5,
-        collection: {
-          overall: [{}],
-          food: [{}],
-          service: [{}],
-          quality: [{}],
-          valueForMoney: [{}],
-        },
-      },
-      reviews: [{}],
-      address: props.resto.address,
-      openingHours: props.resto.openingHours ? props.resto.openingHours : null,
-      photos: [{}],
-    };
+  // function HandleUndefined() {
+  //   const newDocData = {
+  //     views: 0,
+  //     ratings: {
+  //       star: 5,
+  //       collection: {
+  //         overall: [{}],
+  //         food: [{}],
+  //         service: [{}],
+  //         quality: [{}],
+  //         valueForMoney: [{}],
+  //       },
+  //     },
+  //     reviews: [{}],
+  //     address: props.resto.address,
+  //     openingHours: props.resto.openingHours ? props.resto.openingHours : null,
+  //     photos: [{}],
+  //   };
 
-    setDoc(docRef, newDocData)
-      .then((res) => {
-        unsubscribe();
-      })
-      .catch((err) => {
-        console.log(err, props.resto.id);
-      });
-  }
-
-  // function FetchData() {
-
-  const unsubscribe = onSnapshot(docRef, (doc) => {
-    if (doc.data()) {
-      setCloudData(doc.data());
-    } else {
-      HandleUndefined();
-    }
-  });
+  //   setDoc(docRef, newDocData)
+  //     .then((res) => {
+  //       unsubscribe();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err, props.resto.id);
+  //     });
   // }
 
-  useEffect(() => {
-    // console.log(FetchCloudData(props.resto));
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  // // function FetchData() {
 
+  // const unsubscribe = onSnapshot(docRef, (doc) => {
+  //   if (doc.data()) {
+  //     setCloudData(doc.data());
+  //   } else {
+  //     HandleUndefined();
+  //   }
+  // });
+  // // }
+
+  // useEffect(() => {
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, []);
+ 
   return (
     <>
       <Rating
-        ratingValue={(cloudData?.ratings?.star / 5) * 500}
+        ratingValue={(t?.ratings?.star / 5) * 500}
         readonly="true"
         size="25px"
       />
-      <p>views: {cloudData?.views}</p>
+      <p>views: {t?.views}</p>
     </>
   );
 }
