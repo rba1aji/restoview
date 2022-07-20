fbb053985a20018cfd6e2261a6bc912adf5212c0
rba1aji <rjibalaji@gmail.com>
2022-07-21 02:18:58 +0530

    1 file modified

--- src/pages/RestaurantPage/DetailedRatings.js
+++ src/pages/RestaurantPage/DetailedRatings.js
@@ -20,7 +20,7 @@ export default function DetailedRating(props) {
       console.log(i.val);
       tot += parseInt(i.val);
     });
-    ratings.push(tot);
+    ratings.push({key:tot});
   });
   console.log(ratings);
 
