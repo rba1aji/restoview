e88ef94e8854d874b6219806fcbc6a1541dc4100
rba1aji <rjibalaji@gmail.com>
2022-07-20 22:53:14 +0530

    1 file modified

--- src/pages/RestaurantPage/DetailedRatings.js
+++ src/pages/RestaurantPage/DetailedRatings.js
@@ -37,7 +37,7 @@ export default function DetailedRating() {
         'Overall',
         'Food',
         'Service',
-        'Atmosphere',
+        'Ambience',
         `Value \nfor money`,
       ],
     },
