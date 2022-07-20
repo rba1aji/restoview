572b4e4319c2ac7f9ff9604ee8ca30b8b9349b8d
rba1aji <rjibalaji@gmail.com>
2022-07-20 13:21:21 +0530

    apexcharts 🥲

--- src/pages/RestaurantPage/DetailedRatings.js
+++ src/pages/RestaurantPage/DetailedRatings.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { AppState } from '../../reducers/AppContext';
+import ReactApexChart from 'apexcharts';
 
-export default function DetailedRating(){
-  return;
+export default function DetailedRating() {
+  const [series, setSeries] = [
+    {
+      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+    },
+  ];
+
+  return (
+    <ReactApexChart options={options} series={series} type="bar" height={350} />
+  );
 }
