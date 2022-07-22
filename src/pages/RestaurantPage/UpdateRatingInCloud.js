542ecb3d935ffcf6125cd8e2041c981a44ca7af7
rba1aji <rjibalaji@gmail.com>
2022-07-22 12:01:15 +0530

    1 file modified

--- src/pages/RestaurantPage/UpdateRatingInCloud.js
+++ src/pages/RestaurantPage/UpdateRatingInCloud.js
@@ -1,7 +1,40 @@
 import { db } from '../../configs/firebaseConfig';
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
 
 export default function UpdateRatingInCloud(props) {
-  const docRef = doc(db, 'restaurants', props.id);
   console.log(props);
+  const docRef = doc(db, 'restaurants', props.id);
+  updateDoc(docRef, {
+    reviews: arrayUnion(props.review),
+    ratings: {
+      types: {
+        ambience: arrayUnion({
+          uId: props.uId,
+          val: props.ratings.ambience,
+        }),
+        service: arrayUnion({
+          uId: props.uId,
+          val: props.ratings.service,
+        }),
+        food: arrayUnion({
+          uId: props.uId,
+          val: props.ratings.food,
+        }),
+        valueForMoney: arrayUnion({
+          uId: props.uId,
+          val: props.ratings.valueForMoney,
+        }),
+        overall: arrayUnion({
+          uId: props.uId,
+          val: props.ratings.overall,
+        }),
+      },
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
