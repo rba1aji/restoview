7d3f8d407eec5c0ce03767ad54814e24292cf2d4
rba1aji <rjibalaji@gmail.com>
2022-07-27 10:49:17 +0530

    1 file modified

--- src/pages/Famous/TopRated.js
+++ src/pages/Famous/TopRated.js
@@ -3,18 +3,17 @@ import { db, storage } from '../../configs/firebaseConfig';
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
 export default function TopRated(props) {
-  const [numImg, setnumImg] = useState([]);
+  const [numImg, setNumImg] = useState([]);
+  // const numImg=[];
   for (let i = 0; i < 10; i++) {
     getDownloadURL(ref(storage, `Top10/${i}.jpg`))
       .then((url) => {
-        console.log(url);
-        setnumImg((old)=>{
-          return [...old,<img src={url}>];
-        });
+        console.log(numImg);
+        numImg.push(<img src={url}/>);
       })
       .catch((error) => {
         console.log(error.message);
       });
   }
-  return <></>;
+  return ;
 }
