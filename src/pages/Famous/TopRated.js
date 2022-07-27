f020970c9c033f0908958984c33e17e774cd43e2
rba1aji <rjibalaji@gmail.com>
2022-07-27 11:38:26 +0530

    1 file modified

--- src/pages/Famous/TopRated.js
+++ src/pages/Famous/TopRated.js
@@ -1,19 +1,45 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import { db, storage } from '../../configs/firebaseConfig';
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
+function Show(props){
+  console.log(props.numImg.length);
+  return (
+    <div>
+    {
+      props.numImg.map((res,index)=>{
+        return <img key={index} src={res.url} style={{width:'100%'}}/>;
+      })
+    }
+    </div>
+  )
+}
+
 export default function TopRated(props) {
   const [numImg, setNumImg] = useState([]);
-  // const numImg=[];
-  for (let i = 0; i < 10; i++) {
-    getDownloadURL(ref(storage, `Top10/${i}.jpg`))
-      .then((url) => {
-        console.log(numImg);
-        numImg.push(<img src={url}/>);
-      })
-      .catch((error) => {
-        console.log(error.message); 
-      });
+
+  function getNumberImagesFromStorage(){
+    for (let i = 0; i < 10; i++) {
+      getDownloadURL(ref(storage, `Top10/${i}.jpg`))
+        .then((url) => {
+          setNumImg((old)=>{
+            return [...old, {key:i, url:url}];
+          })
+        })
+        .catch((error) => {
+          console.log(error.message); 
+        });
+    }
+    numImg.sort();
   }
-  return ;
+  
+  useEffect(()=>{
+    getNumberImagesFromStorage();
+  },[])
+
+  return (
+    <>
+    {numImg.length>9&&<Show numImg={numImg}/>
+}</>
+  )
 }
