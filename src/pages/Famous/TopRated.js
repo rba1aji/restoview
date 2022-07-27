cd0354b4d7dd1ed731b9e430e503a233b361d22d
rba1aji <rjibalaji@gmail.com>
2022-07-27 09:49:14 +0530

    1 file modified

--- src/pages/Famous/TopRated.js
+++ src/pages/Famous/TopRated.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { db, storage } from '../../configs/firebaseConfig';
-import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
+import { getStorage, ref, listAll } from 'firebase/storage';
 
 export default function TopRated(props) {
   const listRef = ref(storage, 'Top10');
 
-  listAll(listRef) 
+  storage
+    .ref()
+    .child('Top10')
+    .listAll()
     .then((res) => {
-      console.log(res)
-      res.items.forEach((itemRef) => {
+      console.log(res);
+      res.items.map((itemRef) => {
         console.log(itemRef.getDownloadURL());
       });
-    }) 
+    })
     .catch((error) => {
       console.log(error.message);
     });
