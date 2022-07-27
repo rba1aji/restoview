3ed694b9e85547edffd66c8510aa6ea8e969b3d6
rba1aji <rjibalaji@gmail.com>
2022-07-27 17:19:18 +0530

    fetch api data

--- src/pages/Famous/TopRated.js
+++ src/pages/Famous/TopRated.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { db, storage } from '../../configs/firebaseConfig';
-import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  limit,
+} from 'firebase/firestore';
 
 function Show(props) {
   // console.log(props.numImg)
@@ -16,24 +23,29 @@ function Show(props) {
 export default function TopRated(props) {
   const [restos, setRestos] = useState([]);
 
+  function fetchAPIData(id){
+    
+  }
+
   function fetchTopRated() {
     console.log(props.state);
 
     const collectionRef = collection(db, 'restaurants');
     const q =
       props.state == 'India'
-        ? query(collectionRef, orderBy('ratings.star', 'desc'))
+        ? query(collectionRef, orderBy('ratings.star', 'desc'), limit(10))
         : query(
             collectionRef,
             where('address.state', '==', props.state),
-            orderBy('ratings.star', 'desc')
+            orderBy('ratings.star', 'desc'),
+            limit(10)
           );
 
     getDocs(q)
       .then((res) => {
         res.docs.map((doc) => {
           setRestos((old) => {
-            return [...old, { id: doc.id, cloudData: doc.data() }];
+            return [...old, { id: doc.id, cloudData: doc.data(), APIData:fetchAPIData(doc.id) }];
           });
         });
         console.log(restos);
