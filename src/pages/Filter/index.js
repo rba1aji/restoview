7453266223fc335685c792da1e3f9a1901206a33
rba1aji <rjibalaji@gmail.com>
2022-08-03 16:07:29 +0530

    custom toggle fire

--- src/pages/Filter/index.js
+++ src/pages/Filter/index.js
@@ -5,15 +5,66 @@ import {
   FloatingLabel,
   Form,
   ButtonGroup,
+  Dropdown,
+  // CustomToggle,
+  // CustomMenu,
 } from 'react-bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Filter() {
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <Button
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+      variant="outline-dark"
+      style={{ width: '100%' }}
+      className="bg-white"
+    >
+      {children}
+      &#x25bc;
+    </Button>
+  ));
+
+  // forwardRef again here!
+  // Dropdown needs access to the DOM of the Menu to measure it
+  const CustomMenu = React.forwardRef(
+    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+      const [value, setValue] = useState('');
+
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
+        >
+          <Form.Control
+            autoFocus
+            className="mx-3 my-2 w-auto"
+            placeholder="Type to filter..."
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+          <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+              (child) =>
+                !value || child.props.children.toLowerCase().startsWith(value)
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
+
   return (
     <>
       <h1>Filter</h1>
       <p className="text-center opacity-75">Find the restaurant you want</p>
-      <br/>
+      <br />
       <Table bordered>
         <tbody>
           <tr>
@@ -48,13 +99,17 @@ export default function Filter() {
               </FloatingLabel>
             </td>
             <td>
-              <DropdownButton
-                title={`Sort by`}
-                variant="outline-dark"
-                style={{ width: '100%' }}
-                as={ButtonGroup}
-                className="bg-white"
-              ></DropdownButton>
+              <Dropdown>
+                <Dropdown.Toggle
+                  as={CustomToggle}
+                  id="dropdown-custom-components"
+                >
+                  State
+                </Dropdown.Toggle>
+                <Dropdown.Menu as={CustomMenu}>
+                  <Dropdown.Item>africa</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </td>
           </tr>
         </tbody>
